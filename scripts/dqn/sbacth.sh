d0a7b36dc596639bcff4cbe049a9cb2bfc411a5b
Mary <mazo2428@colorado.edu>
2020-05-28 13:46:15 -0400

    run more scripts

---
+++ scripts/dqn/sbacth.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH -p localLimited
+#SBATCH -A ecortex
+#SBATCH --mem=25G
+#SBATCH --time=72:00:00
+#SBATCH -c 2
+
+
+sbatch dqn_pong_deafults_lr2.sh
+sbatch dqn_pong_deafults_lr4.sh
+sbatch qn_pong_RMSprop_bacth8_lr4.sh
+sbatch dqn_pong_RMSprop_lr4.sh
